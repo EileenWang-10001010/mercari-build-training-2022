2fbff99cbcccb104c2c2694538df47fe92c3dc88
Ai-Lin Wang <eileenljvms@gmail.com>
2022-05-13 20:03:36 +0800

    C:/Program Files/Git/items POST&GET if json file is empty

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,8 +23,21 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+@app.get("/items")
+def return_item():
+    with open('items.json') as f:
+        items = json.load(f)
+        f.close()
+
+        if items =="[]":
+            items = {"items":[{"name":"jacket", "category": "fashion"}]}
+            # items["items"].append({"name":"jacket", "category": "fashion"})
+
+    return {f"{items}"}
+    # return json.dumps(items)
+
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...), category: str =Form(...)):
     logger.info(f"Receive item: {name}")
     return {"message": f"item received: {name}"}
 
