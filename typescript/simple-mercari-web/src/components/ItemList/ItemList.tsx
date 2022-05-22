fd4fd9645e59c6b32c0ac32ed95719d2c3b453ae
Eileen <eileenljvms@gmail.com>
2022-05-22 22:47:31 +0800

    step5

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -4,10 +4,13 @@ interface Item {
   id: number;
   name: string;
   category: string;
-  image: string;
+  image: string | File;
 };
 
+
+
 const server = process.env.API_URL || 'http://127.0.0.1:9000';
+
 const placeholderImage = process.env.PUBLIC_URL + '/logo192.png';
 
 export const ItemList: React.FC<{}> = () => {
@@ -24,7 +27,8 @@ export const ItemList: React.FC<{}> = () => {
     })
       .then(response => response.json())
       .then(data => {
-        console.log('GET success:',data);
+        console.log('GET success:',data.items);
+        console.log(process.env);
         setItems(data.items);
       })
       .catch(error => {
@@ -37,14 +41,14 @@ export const ItemList: React.FC<{}> = () => {
   }, []);
   
   return (
-    <div>
+    <div style={{ backgroundColor: '#222427' }}>
       { items.map((item) => {
         return (
-          <div key={item.id} className='ItemList'>
+          <div key={item.id} className='ItemList' >
             {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img src={placeholderImage}/>
+            <img  style={{ width: '50%' }} src={server.concat(`/image/${item.image}`)} alt={item.name} />
             <p>
-            <span>Name: {item.name}</span>
+            <span >Name: {item.name}</span>
             <br/>
             <span>Category: {item.category}</span>
             </p>
--- typescript/simple-mercari-web/src/components/Listing/Listing.tsx
+++ typescript/simple-mercari-web/src/components/Listing/Listing.tsx
@@ -2,19 +2,31 @@ import React, { useState } from 'react';
 
 const server = process.env.API_URL || 'http://127.0.0.1:9000';
 
+type formType = {
+  name: string,
+  category: string,
+  image:  File | string,
+}
+
 export const Listing: React.FC<{}> = () => {
   const initialState = {
     name: "",
     category: "",
     image: "",
   };
-  const [values, setValues] = useState(initialState);
   
+  const [values, setValues] = useState<formType>(initialState);
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       setValues({ ...values, [event.target.name]: event.target.value });
   };
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.files![0] });
+  };
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+   
     const data = new FormData()
     data.append('name', values.name)
     data.append('category', values.category)
@@ -28,6 +40,7 @@ export const Listing: React.FC<{}> = () => {
     .then(response => response.json())
     .then(data => {
       console.log('POST success:', data);
+      setValues(initialState);
     })
     .catch((error) => {
       console.error('POST error:', error);
@@ -39,7 +52,7 @@ export const Listing: React.FC<{}> = () => {
         <div>
             <input type='text' name='name' id='name' placeholder='name' onChange={onChange} required/>
             <input type='text' name='category' id='category' placeholder='category' onChange={onChange}/>
-            <input type='file' name='image' id='image' placeholder='image' onChange={onChange}/>
+            <input type='file' name='image' id='image' placeholder='image' onChange={onFileChange}/>
             <button type='submit'>List this item</button>
         </div>
       </form>
